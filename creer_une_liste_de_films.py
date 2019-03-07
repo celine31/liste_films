9cf688e556fdb36ad662c353721b0292d65dc11f
celine31 <dadou20231@gmail.com>
2019-03-07 16:24:33 +0100

    version avec les tuples

--- creer_une_liste_de_films.py
+++ creer_une_liste_de_films.py
@@ -3,14 +3,15 @@ films=[]
 while continuer=='o':
 	
 	#Demander a l'utilisateur de rentrer des noms de films
-	film=raw_input('Entrez un nom de film : ')
-
+	film_a_ajouter=raw_input('Entrez un nom de film : ')
+	
 	#ajouter ces films dans la liste
 	#gerer les doublons
-	if film.lower() in [film.lower() for film in films]:
-		print('le film est deja existant dans la liste')
+	if film_a_ajouter.lower() in [film[0].lower() for film in films]:
+		print('{0} est deja existant dans la liste'.format(film_a_ajouter))
 	else:
-		films.append(film)
+		note= (raw_input('entrez une note de 1 a 5 : '))
+		films.append((film_a_ajouter,note))
 
 	continuer=raw_input('voulez vous continuer? : o/n ')
 
